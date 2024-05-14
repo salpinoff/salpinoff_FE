c5e68977e033f272a137dac72970cc968ac649fd
emayom <emayom7@gmail.com>
2024-05-15 06:26:33 +0900

    🐛 fix: 토큰 파일 재정의

--- src/tokens/shadows.ts
+++
@@ -1,9 +0,0 @@
-export const SHADOWS = {
-  0: '0px 0px 0px 0px #00000000',
-  1: '0px 1px 3px -1px #00000029',
-  2: '0px 3px 4px -2px #00000029',
-  3: '0px 8px 24px -4px #0000001F',
-  4: '0px 18px 28px -8px #00000014',
-  5: '4px 16px 40px -8px #0000001A',
-  6: '6px 32px 48px -8px #0000001A',
-};
