bf5c2cd7685add24e25cc9ee9e4d5cbd6a3f2bf2
emayom <emayom7@gmail.com>
2024-05-16 05:44:10 +0900

    ✨ feat: tailwindcss.theme.extend 토큰 적용 close #6

--- tokens/parsed-tokens/index.ts
+++ tokens/parsed-tokens/index.ts
@@ -1,20 +1,28 @@
 import type { DesignTokens } from 'style-dictionary';
 import type { ThemeConfig } from 'tailwindcss/types/config';
 
-import { color } from '../color/base.json';
+import { color as color_tokens } from '../color/base.json';
+import { borderRadius as radius_tokens } from '../radius.json';
+import { shadow as shadow_tokens } from '../shadow.json';
+import { spacing as spacing_tokens } from '../spacing.json';
 
 /**
  * Migrate from token based on 'style-dictionary' to object
  */
-const parseBaseColorToken = (tokens: DesignTokens): ThemeConfig['colors'] =>
+const parseToken = <T extends keyof ThemeConfig>(
+  tokens: DesignTokens,
+): ThemeConfig[T] =>
   Object.entries(tokens).reduce(
     (acc, [k, v]) => ({
       ...acc,
-      [k]: Object.hasOwn(v, 'value') ? v.value : parseBaseColorToken(v),
+      [k]: Object.hasOwn(v, 'value') ? v.value : parseToken(v),
     }),
     {},
   );
 
-const colors = parseBaseColorToken(color.base);
+const colors = parseToken<'colors'>(color_tokens.base);
+const spacing = parseToken<'spacing'>(spacing_tokens);
+const boxShadow = parseToken<'boxShadow'>(shadow_tokens);
+const borderRadius = parseToken<'borderRadius'>(radius_tokens);
 
-export { colors };
+export { colors, spacing, boxShadow, borderRadius };
