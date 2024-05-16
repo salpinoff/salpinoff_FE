8313e2b69ffa2da2e233f7a9d1051bef724958e8
emayom <emayom7@gmail.com>
2024-05-17 03:37:00 +0900

    ✨ feat: typography helper classes 정의를 위한 함수 추가

--- tokens/parsed-tokens/index.ts
+++ tokens/parsed-tokens/index.ts
@@ -1,10 +1,11 @@
 import type { DesignTokens } from 'style-dictionary';
-import type { ThemeConfig } from 'tailwindcss/types/config';
+import type { ThemeConfig, PluginAPI } from 'tailwindcss/types/config';
 
 import { color as color_tokens } from '../color/base.json';
 import { borderRadius as radius_tokens } from '../radius.json';
 import { shadow as shadow_tokens } from '../shadow.json';
 import { spacing as spacing_tokens } from '../spacing.json';
+import { typography as typography_tokens } from '../typography.json';
 
 /**
  * Migrate from token based on 'style-dictionary' to object
@@ -20,9 +21,35 @@ const parseToken = <T extends keyof ThemeConfig>(
     {},
   );
 
+/**
+ * Migrate from token based on 'style-dictionary' to CSS rule object
+ * [tailwindcss/plugin](https://tailwindcss.com/docs/plugins#adding-components)
+ */
+const parseTypographyToken = (
+  tokens: DesignTokens,
+  prefix?: string,
+): Parameters<PluginAPI['addComponents']>[0] =>
+  Object.entries(tokens).reduce(
+    (acc, [k, v]) => ({
+      ...acc,
+      ...(Object.hasOwn(v, 'value')
+        ? { [`.${[prefix, k].join('-')}`]: v.value }
+        : parseTypographyToken(v, k)),
+    }),
+    {},
+  );
+
+/**
+ * [TO-BE] Tailwindcss theme propeties
+ */
 const colors = parseToken<'colors'>(color_tokens.base);
 const spacing = parseToken<'spacing'>(spacing_tokens);
 const boxShadow = parseToken<'boxShadow'>(shadow_tokens);
 const borderRadius = parseToken<'borderRadius'>(radius_tokens);
 
-export { colors, spacing, boxShadow, borderRadius };
+/**
+ * [TO-BE] Typography helper classes
+ */
+const typography = parseTypographyToken(typography_tokens);
+
+export { colors, spacing, boxShadow, borderRadius, typography };
