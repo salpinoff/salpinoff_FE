7fd63dfe75a9b9515dcef0f5e238bd61adf382db
emayom <emayom7@gmail.com>
2024-05-20 01:05:06 +0900

    ✨ feat: FormLabel 컴포넌트 구현 #15

---
+++ src/app/components/common/FormLabel/index.tsx
@@ -0,0 +1,39 @@
+import { PropsWithChildren } from 'react';
+
+import { cva, cx, type VariantProps } from 'class-variance-authority';
+
+import { RequiredByKeys } from '@type/util';
+
+export type LabelProps = VariantProps<typeof label>;
+
+const label = cva(['label-1-regular'], {
+  variants: {},
+});
+
+export type FormLabelProps<T extends React.ElementType = 'label'> =
+  RequiredByKeys<
+    PropsWithChildren<LabelProps> &
+      React.ComponentPropsWithoutRef<T> & {
+        required?: boolean;
+      },
+    'id' | 'children'
+  >;
+
+export default function FormLabel<T extends React.ElementType = 'label'>({
+  id,
+  className,
+  children,
+  required,
+  ...rest
+}: FormLabelProps<T>) {
+  return (
+    <label htmlFor={id} className={cx(label({}), className)} {...rest}>
+      <div>
+        {children}
+        {required && (
+          <span className="pl-4 text-[--color-text-accent-red]">*</span>
+        )}
+      </div>
+    </label>
+  );
+}
