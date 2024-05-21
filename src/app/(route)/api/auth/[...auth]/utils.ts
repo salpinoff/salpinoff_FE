f987ff07725558191a5a4726e21a3133d9e82885
윤석규 <sg.yun@joongonara.co.kr>
2024-05-21 19:38:19 +0900

    ♻️ refactor: setCookie 함수 분리
    - 반복 사용 가능한 setCookie 함수 분리

--- src/app/(route)/api/auth/[...auth]/utils.ts
+++ src/app/(route)/api/auth/[...auth]/utils.ts
@@ -1,19 +1,11 @@
 import { NextResponse } from 'next/server';
 
-import { type Response as TypeResponse } from '@api/auth/token';
+import { type Response as TypeResponse } from '@api/auth/token/get-token';
 
-const redirectResponse = ({
-  code,
-  username,
-  accessToken,
-  refreshToken,
-}: TypeResponse) => {
-  const destinationUrl =
-    (code === 100 && `/signUp?code=${code}`) ||
-    (code === 102 && `/signUp?code=${code}&user=${username}`) ||
-    '/';
-
-  const response = NextResponse.redirect(destinationUrl, { status: 302 });
+const setCookie = (
+  response: NextResponse,
+  { accessToken, refreshToken }: { refreshToken: string; accessToken: string },
+) => {
   const cookies = [
     {
       key: 'accessToken',
@@ -36,4 +28,19 @@ const redirectResponse = ({
   return response;
 };
 
-export default redirectResponse;
+const redirectResponse = ({
+  code,
+  username,
+  accessToken,
+  refreshToken,
+}: TypeResponse) => {
+  const destinationUrl =
+    (code === 100 && `/signUp?code=${code}`) ||
+    (code === 102 && `/signUp?code=${code}&user=${username}`) ||
+    '/';
+
+  const response = NextResponse.redirect(destinationUrl, { status: 302 });
+  return setCookie(response, { accessToken, refreshToken });
+};
+
+export { redirectResponse, setCookie };
