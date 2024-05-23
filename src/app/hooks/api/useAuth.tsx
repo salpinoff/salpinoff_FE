24de1a0f8bf9783d261bfb7d1f0e6758d8cb728d
윤석규 <sg.yun@joongonara.co.kr>
2024-05-23 19:56:34 +0900

    ✨ feat: useAuth 훅 생성 #23
    - 클라이언트 사이드에서 로그인 status 확인가능한 훅
    - 필요한 경우 session 업데이트 할 수 있는 mutate 함수 반환

--- src/app/hooks/api/useAuth.tsx
+++ src/app/hooks/api/useAuth.tsx
@@ -1,16 +1,50 @@
-import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
-import { getSession } from '@api/auth/session';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-const useAuth = () => {
-  return useQuery({
+import { apiInstance } from '@api/api.config';
+import { getSession, updateSession } from '@api/auth/session';
+
+const useAuth = (): {
+  status: 'loading' | 'authenticated' | 'unauthenticated' | 'error';
+  accessToken: string;
+  update: () => void;
+} => {
+  const queryClient = useQueryClient();
+
+  const { data: session, error } = useQuery({
     queryKey: ['authToken'],
     queryFn: getSession,
-    retry: 1,
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
+    retry: false,
+    staleTime: 1000 * 60 * 5, // 5분 동안 토큰이 신선하다고 간주
+    gcTime: 1000 * 60 * 10, // 10분 동안 캐시 유지
     select: (data) => data.data,
   });
+
+  const { mutate: update } = useMutation({
+    mutationKey: ['updateToken'],
+    mutationFn: updateSession,
+    onSuccess: ({ data: { accessToken } }) => {
+      queryClient.setQueryData(['authToken'], {
+        accessToken,
+        status: 'authenticated',
+      });
+    },
+  });
+
+  useEffect(() => {
+    if (session?.accessToken) {
+      apiInstance.defaults.headers.Authorization = `Bearer ${session.accessToken}`;
+    } else {
+      delete apiInstance.defaults.headers.Authorization;
+    }
+  }, [session]);
+
+  return {
+    status: (error && 'error') || session?.status || 'loading',
+    accessToken: session?.accessToken || '',
+    update,
+  };
 };
 
 export default useAuth;
