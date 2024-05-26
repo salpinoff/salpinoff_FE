d928bc44f03fe7a19aa943bc2880930e57a4aae4
emayom <emayom7@gmail.com>
2024-05-27 04:19:25 +0900

    🐛 fix: safari grid 오류 수정

--- src/app/components/common/Modal/ModalContent.tsx
+++ src/app/components/common/Modal/ModalContent.tsx
@@ -7,7 +7,7 @@ const ModalContent = forwardRef<
   return (
     <div
       ref={ref}
-      className="absolute bottom-0 left-0 right-0 top-0 z-50 m-auto grid h-max w-[312px] gap-24 rounded-20 bg-[#141415] p-24"
+      className="absolute bottom-0 left-0 right-0 top-0 z-50 m-auto flex h-max max-h-fit w-[312px] flex-col gap-24 rounded-20 bg-[#141415] p-24"
     >
       {children}
     </div>
