0c5b4a318ceb0481315546abc2b912da5d73448d
윤석규 <sg.yun@joongonara.co.kr>
2024-05-26 20:34:15 +0900

    ✨ feat: 스트레스 정도 선택 컴포넌트 생성 #9

---
+++ src/app/(route)/(auth)/signup/components/SelectStress.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { ChangeEvent, useState } from 'react';
+
+import FormHelperText from '@components/common/TextField/FormHelperText';
+import Slider from '@components/Slider';
+
+import useSignUpContext from '../hooks/useSignUpContext';
+
+const slider = {
+  step: 1,
+  min: 1,
+  max: 100,
+};
+
+function SelectStress() {
+  const { min, max, step } = slider;
+  const [stress, setStress] = useState(min);
+
+  const { setBtnDisabled } = useSignUpContext();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { target } = e;
+    const { value } = target;
+
+    setStress(Number(value));
+    setBtnDisabled(false);
+  };
+
+  return (
+    <div>
+      <FormHelperText className="mb-[30px] block">스트레스 정도</FormHelperText>
+      <Slider
+        min={min}
+        max={max}
+        step={step}
+        displayInterval
+        className="mb-48"
+        onChange={handleChange}
+      />
+
+      <span className="m-auto flex h-[48vw] w-[48vw] items-center justify-center rounded-circular bg-[var(--color-base-cool-neutral-7)]">
+        <span className="display-1-bold text-white">{stress}</span>
+      </span>
+    </div>
+  );
+}
+
+export default SelectStress;
