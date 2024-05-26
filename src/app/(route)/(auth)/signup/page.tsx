ef6a99172f8b48251e3b4a87b42fcbbde436fc34
윤석규 <sg.yun@joongonara.co.kr>
2024-05-26 21:59:07 +0900

    ✨ feat: 로그인 페이지 구현 #9

--- src/app/(route)/(auth)/signup/page.tsx
+++ src/app/(route)/(auth)/signup/page.tsx
@@ -1,7 +1,59 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+import useFunnel from 'src/app/hooks/useFunnel';
+
 import MakeNickName from './components/MakeNickName';
+import SelectEmotion from './components/SelectEmotion';
+import SignUpLayout from './components/SignUpLayout';
+import { title } from './constant/funnel';
+
+const SelectStress = dynamic(() => import('./components/SelectStress'));
+const WriteStory = dynamic(() => import('./components/WriteStory'));
 
 function SignUp() {
-  return <MakeNickName />;
+  const { Funnel, setStep } = useFunnel('nickname');
+
+  return (
+    <Funnel>
+      <Funnel.Step name="nickname">
+        <SignUpLayout title={title.nickname} goNext={() => setStep('emotion')}>
+          <MakeNickName />
+        </SignUpLayout>
+      </Funnel.Step>
+
+      <Funnel.Step name="emotion">
+        <SignUpLayout
+          title={title.emotion}
+          goPrev={() => setStep('nickname')}
+          goNext={() => setStep('stress')}
+        >
+          <SelectEmotion />
+        </SignUpLayout>
+      </Funnel.Step>
+
+      <Funnel.Step name="stress">
+        <SignUpLayout
+          title={title.stress}
+          goPrev={() => setStep('emotion')}
+          goNext={() => setStep('story')}
+        >
+          <SelectStress />
+        </SignUpLayout>
+      </Funnel.Step>
+
+      <Funnel.Step name="story">
+        <SignUpLayout
+          title={title.story}
+          goPrev={() => setStep('stress')}
+          goNext={() => setStep('monstername')}
+        >
+          <WriteStory />
+        </SignUpLayout>
+      </Funnel.Step>
+    </Funnel>
+  );
 }
 
 export default SignUp;
