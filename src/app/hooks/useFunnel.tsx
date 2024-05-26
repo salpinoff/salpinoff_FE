014f0eea42b96f749286e1056bf175dc110deb4e
윤석규 <sg.yun@joongonara.co.kr>
2024-05-26 20:40:27 +0900

    ✨ feat: useFunnel 훅 생성 #9
    - step 상태에 따라 분기 처리가 가능한 Funnel 컴포넌트와 이를 업데이트할 수 있는 setStep 반환

---
+++ src/app/hooks/useFunnel.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable react/jsx-no-useless-fragment */
+import {
+  Children,
+  PropsWithChildren,
+  ReactElement,
+  isValidElement,
+  useState,
+} from 'react';
+
+import { funnel } from '../(route)/(auth)/signup/constant/funnel';
+
+type StepKey = (typeof funnel)[number];
+type StepProps = PropsWithChildren<{ name: StepKey }>;
+type FunnelProps = {
+  children: ReactElement<StepProps> | ReactElement<StepProps>[];
+};
+
+const useFunnel = (defaultStep: StepKey) => {
+  const [step, setStep] = useState(defaultStep);
+
+  function Funnel({ children }: FunnelProps): ReactElement {
+    const targetStep = Children.toArray(children)
+      .filter(isValidElement<StepProps>)
+      .find((child) => child.props.name === step);
+
+    return <>{targetStep}</>;
+  }
+
+  function Step({ children }: StepProps): ReactElement {
+    return <>{children}</>;
+  }
+
+  Funnel.Step = Step;
+
+  return { Funnel, setStep };
+};
+
+export default useFunnel;
