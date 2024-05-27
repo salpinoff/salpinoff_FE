3990656012d7381b0042659c9eb6092ccdeca835
윤석규 <sg.yun@joongonara.co.kr>
2024-05-27 18:28:46 +0900

    ✨ feat: query dev tool 설치

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -3,6 +3,7 @@
 import { PropsWithChildren } from 'react';
 
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { getQueryClient } from '@utils/query';
 
@@ -12,7 +13,10 @@ function QueryProvider({ children }: Props) {
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
