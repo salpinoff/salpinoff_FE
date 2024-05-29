4e004c9bfbe853215a5c96d067796f802ef07145
emayom <emayom7@gmail.com>
2024-05-30 06:53:30 +0900

    🎨 style: AuthLayout CSS 클래스 수정

--- src/app/(route)/(auth)/layout.tsx
+++ src/app/(route)/(auth)/layout.tsx
@@ -8,7 +8,11 @@ function AuthLayout({ children }: Props) {
   return (
     <main
       style={{ touchAction: 'auto' }}
-      className={cn('full-height', 'relative box-border flex flex-col')}
+      className={cn(
+        // RootLayout (375*812)
+        // 이후 프로젝트 전체에 반영되어야 하는 레이아웃
+        'relative m-auto box-border	flex h-dvh min-h-[812px] w-[375px] flex-col items-center overscroll-none p-20',
+      )}
     >
       {children}
     </main>
