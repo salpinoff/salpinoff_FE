fe38b10de91a9b69a0ba3ec399206ae4af39f577
윤석규 <sg.yun@joongonara.co.kr>
2024-06-01 16:33:28 +0900

    🐛 fix: nickname API url 수정

--- src/app/api/auth/nickname.ts
+++ src/app/api/auth/nickname.ts
@@ -1,13 +1,13 @@
 import { apiInstance } from '@api/api.config';
 
 const createUserName = async (username: string) => {
-  const path = '/members/my';
+  const path = '/api/v1/members/my';
 
   return apiInstance.post(path, { username });
 };
 
 const modifyUserName = async (username: string) => {
-  const path = '/members/my';
+  const path = '/api/v1/members/my';
 
   return apiInstance.put(path, { username });
 };
