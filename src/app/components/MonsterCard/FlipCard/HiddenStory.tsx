01b339e4fac2ef69537b8c08b92e7aec043628fa
emayom <emayom7@gmail.com>
2024-06-04 08:40:28 +0900

    ✨ feat: FlipCard/HiddenStory 컴포넌트 구현 #35

---
+++ src/app/components/MonsterCard/FlipCard/HiddenStory.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/navigation';
+
+import Button from '@components/common/Button';
+import BaseText from '@components/common/Text/BaseText';
+
+type HiddenStoryTypes = React.PropsWithChildren<{
+  owner?: boolean;
+}>;
+
+export default function HiddenStory({
+  children,
+  owner = false,
+}: HiddenStoryTypes) {
+  const { replace } = useRouter();
+
+  const handleClick = () => {
+    // [TODO] 내용 수정 페이지 경로로 변경
+    replace('/');
+  };
+
+  return (
+    <div className="flex h-full select-none flex-col gap-20 rounded-[28px] bg-cool-neutral-17 p-20 shadow-5">
+      <BaseText
+        className="my-auto flex max-h-full overflow-y-auto text-center"
+        component="p"
+        variant="body-2"
+        weight="medium"
+        color="strong"
+        wrap
+      >
+        {children}
+      </BaseText>
+      {owner && (
+        <Button
+          variant="secondary"
+          size="small"
+          className="w-full"
+          onClick={handleClick}
+        >
+          내용 수정하기
+        </Button>
+      )}
+    </div>
+  );
+}
