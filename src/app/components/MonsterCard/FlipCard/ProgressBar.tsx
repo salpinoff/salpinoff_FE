fb5eeaa09e56f107f4774230d3a029c88105be9a
emayom <emayom7@gmail.com>
2024-06-08 05:11:21 +0900

    🐛 fix: ProgressBar props 수정 #35
    - ProgressBar 내부에서 연산을 수행하도록 props를 수정하였습니다.

--- src/app/components/MonsterCard/FlipCard/ProgressBar.tsx
+++ src/app/components/MonsterCard/FlipCard/ProgressBar.tsx
@@ -3,22 +3,25 @@ import { motion } from 'framer-motion';
 import BaseText from '@components/common/Text/BaseText';
 
 type ProgressBarProps = {
-  percent: number;
+  value: number;
+  max?: number;
 };
 
-export default function ProgressBar({ percent }: ProgressBarProps) {
+export default function ProgressBar({ value, max = 100 }: ProgressBarProps) {
+  const percent = (Math.min(value / max) * 100).toFixed(1);
+
   return (
     <div className="flex items-center gap-10 last:text-inherit">
-      <div className="h-[14px] w-[194px] rounded-circular bg-cool-neutral-17">
+      <div className="h-[14px] w-[194px] overflow-hidden rounded-circular bg-cool-neutral-17">
         {/* Indicator */}
         <motion.div
           className="h-full rounded-circular bg-current"
           initial={{ width: 0 }}
-          animate={{ width: `${Math.min(percent, 100)}%` }}
+          animate={{ width: `${percent}%` }}
         />
       </div>
       <BaseText component="span" variant="label-1" weight="semibold">
-        {Math.min(percent, 100)}%
+        {percent}%
       </BaseText>
     </div>
   );
