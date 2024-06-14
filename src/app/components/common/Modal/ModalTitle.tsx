ac57ae254b9b190496eebd3d0ad25933cafc36a4
emayom <emayom7@gmail.com>
2024-06-15 03:01:49 +0900

    🐛 fix(common): Modal/ModalTitle Props 수정
    - className, align Props를 받을 수 있도록 추가하였습니다.

--- src/app/components/common/Modal/ModalTitle.tsx
+++ src/app/components/common/Modal/ModalTitle.tsx
@@ -1,12 +1,23 @@
-import { PropsWithChildren } from 'react';
+import BaseText, { BaseTextProps } from '@components/common/Text/BaseText';
 
-import BaseText from '@components/common/Text/BaseText';
+type ModalTitleProps = Pick<
+  BaseTextProps<'h3'>,
+  'children' | 'className' | 'align'
+>;
 
-type ModalTitleProps = PropsWithChildren;
-
-export default function ModalTitle({ children }: ModalTitleProps) {
+export default function ModalTitle({
+  children,
+  className,
+  align = 'left',
+}: ModalTitleProps) {
   return (
-    <BaseText component="h3" variant="headline-1" color="normal">
+    <BaseText
+      className={className}
+      align={align}
+      component="h3"
+      variant="headline-1"
+      color="normal"
+    >
       {children}
     </BaseText>
   );
