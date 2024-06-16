33f2911778032ff989ebc3519c861f39cd8a92cd
윤석규 <sg.yun@joongonara.co.kr>
2024-06-16 21:54:11 +0900

    ♻️ refactor: 메시지 관련 API 타입 별도 파일 분리
    - 타입 재사용을 위해 별도 파일로 분리

---
+++ src/app/api/message/type.ts
@@ -0,0 +1,17 @@
+export type MessageListProps = {
+  page: number;
+  size?: number;
+  monsterId: number;
+};
+
+export type MessageListResponse = {
+  content: {
+    messageId: number;
+    sender: string;
+    content: string;
+    checked: boolean;
+  }[];
+  size: number;
+  page: number;
+  totalElements: number;
+};
