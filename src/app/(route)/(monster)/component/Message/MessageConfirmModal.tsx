f3799f07a4d22eac00edb9b141c0ee10aefb783c
윤석규 <sg.yun@joongonara.co.kr>
2024-06-16 18:22:56 +0900

    ✨ feat: MessageConfirmModal 컴포넌트 생성
    - 메시지를 보낸 사람과 내용을 확인 할 수 있는 모달 컴포넌트

---
+++ src/app/(route)/(monster)/component/Message/MessageConfirmModal.tsx
@@ -0,0 +1,105 @@
+import { MouseEventHandler, useRef } from 'react';
+
+import Heart from '@public/icons/heart.svg';
+
+import Button from '@components/common/Button';
+import Icon from '@components/common/Icon';
+import { Modal } from '@components/common/Modal';
+import BaseText from '@components/common/Text/BaseText';
+
+import useOutsideClick from '@hooks/useOutsideClick';
+
+import cn from '@utils/cn';
+
+type Props = {
+  content: string;
+  sender: string;
+  checked: boolean;
+  closeModal: () => void;
+};
+
+function MessageConfirmModal({ content, sender, checked, closeModal }: Props) {
+  const modalRef = useRef<HTMLDivElement>(null);
+  const handleClick: MouseEventHandler = () => {
+    closeModal();
+  };
+
+  useOutsideClick(modalRef, () => closeModal(), 'mousedown');
+
+  return (
+    <Modal open>
+      <Modal.Dimmed />
+      <Modal.Content className="w-[calc(100vw-64px)] items-center bg-transparent p-0">
+        <section
+          ref={modalRef}
+          className={cn(
+            'flex flex-col items-center justify-between',
+            'aspect-[312/430] w-full rounded-32',
+            'gap-[10px] bg-[#70737C47] px-24 pb-32 pt-[42px] text-white',
+          )}
+        >
+          <BaseText component="h2" variant="heading-1" weight="semibold">
+            {sender}님의 메시지
+          </BaseText>
+
+          <BaseText
+            variant="body-1"
+            weight="regular"
+            className={cn(
+              'flex-1',
+              'flex flex-col items-center justify-start',
+              'overflow-y-auto',
+              'max-h-[32dvh] max-w-full break-all',
+            )}
+          >
+            <span className="my-auto inline-block">{content}</span>
+          </BaseText>
+
+          <div
+            className={cn(
+              'aspect-square h-80',
+              'rounded-20',
+              'flex flex-shrink-0 flex-grow-0 flex-col items-center justify-center gap-[12.5%]',
+              {
+                'bg-[var(--color-base-red-90)]': !checked,
+                'bg-[var(--color-base-cool-neutral-80)]': checked,
+              },
+            )}
+          >
+            <Icon
+              className={cn('block h-[32.5%] w-[80%]', {
+                'text-[var(--color-base-red-50)]': !checked,
+                'text-[var(--color-base-cool-neutral-50)]': checked,
+              })}
+            >
+              <Heart />
+            </Icon>
+
+            <BaseText
+              weight="semibold"
+              variant="caption-1"
+              className={cn({
+                'text-[var(--color-base-red-40)]': !checked,
+                'text-[var(--color-base-cool-neutral-30)]': checked,
+              })}
+            >
+              에너지 40
+            </BaseText>
+          </div>
+        </section>
+
+        <Button
+          onClick={handleClick}
+          className="w-[52.5%]"
+          variant={checked ? 'secondary' : 'primary'}
+        >
+          <BaseText variant="body-2" weight={checked ? 'medium' : 'semibold'}>
+            {checked ? '닫기' : '에너지 적용하기'}
+          </BaseText>
+        </Button>
+      </Modal.Content>
+    </Modal>
+  );
+}
+
+export default MessageConfirmModal;
