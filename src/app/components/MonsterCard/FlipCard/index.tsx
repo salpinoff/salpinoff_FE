7b19bb0a28a72ec65545adf50b3d4d7d2f1a947a
emayom <emayom7@gmail.com>
2024-06-18 07:37:31 +0900

    ♻️ refactor: MonsterFlipCard 일부 컴포넌트 분리 및 적용
    - 분리한 FlipCard 컴포넌트를 적용하기 위해 수정하였습니다.
    - 분리한 OwnerInfo, StressLevelWithName를 적용하기 위해 수정하였습니다.
    - 추가한 Dropdown 컴포넌트를 적용하기 위해 수정하였습니다.

--- src/app/components/MonsterCard/FlipCard/index.tsx
+++ src/app/components/MonsterCard/FlipCard/index.tsx
@@ -6,9 +6,11 @@ import { useEffect, useState } from 'react';
 
 import { useAtom } from 'jotai';
 
-import { AnimatePresence, motion } from 'framer-motion';
+import DownloadSVG from '@public/icons/download.svg';
+import EditSVG from '@public/icons/edit.svg';
+import ShareSVG from '@public/icons/share.svg';
 
-import BaseText from '@components/common/Text/BaseText';
+import FlipCard from '@components/FlipCard';
 import CardBase from '@components/MonsterCard/CardBase';
 
 import { findObjectInArray } from '@utils/find';
@@ -18,153 +20,121 @@ import { monsterAtom } from '@store/monsterAtom';
 import CounterBox from './CounterBox';
 import Dropdown from './Dropdown';
 import HiddenStory from './HiddenStory';
+import OwnerInfo from './OwnerInfo';
 import ProgressBar from './ProgressBar';
+import StressLevelWithName from './StressLevelWithName';
 
 type MonsterFlipCardProps = {
   flip?: boolean;
-  isOwner?: boolean;
+  owner?: boolean;
   onFlipped?: (arg?: unknown) => void;
 };
 
+const DROPDOWN_LIST = [
+  {
+    text: '저장하기',
+    icon: <DownloadSVG />,
+    onClick: () => {
+      // [TODO]: 저장하기 기능 구현
+    },
+  },
+  {
+    text: '공유하기',
+    icon: <ShareSVG />,
+    onClick: () => {
+      // [TODO]: 공유하기 로직 추가
+    },
+  },
+  {
+    text: '수정하기',
+    icon: <EditSVG />,
+    onClick: () => {
+      // [TODO]: 수정하기 페이지 이동 로직 추가
+    },
+  },
+];
+
 export default function MonsterFlipCard({
   flip = false,
-  isOwner = false,
+  owner = false,
   onFlipped,
 }: MonsterFlipCardProps) {
   const [{ data: monster }] = useAtom(monsterAtom);
-
-  const [isFlipped, setIsFlipped] = useState(flip);
-  const [isAnimating, setIsAnimating] = useState(false);
+  const [flipped, setFlipped] = useState(flip);
   const [totalCount, setTotalCount] = useState(flip ? 999 : 0);
 
+  const {
+    ownerName = '',
+    monsterName = '',
+    content = '',
+    ratingRange = '',
+    interactionCountPerEncouragement: interactionCount = 0,
+    monsterDecorations = [],
+  } = monster ?? {};
+
   const findDecoration = findObjectInArray(
-    monster?.monsterDecorations || [],
+    monsterDecorations,
     'decorationType',
   );
 
   const COLOR = findDecoration?.('BACKGROUND_COLOR')?.decorationValue;
 
-  const handleCardToggle = () => {
-    if (!isAnimating) {
-      setIsFlipped(!isFlipped);
-      setIsAnimating(true);
-    }
-  };
+  const toggleCard = () => setFlipped((prev) => !prev);
 
   useEffect(() => {
-    if (monster) {
-      if (totalCount >= monster?.interactionCountPerEncouragement) {
-        setIsFlipped(true);
-
-        onFlipped?.();
-      }
+    if (interactionCount && totalCount >= interactionCount) {
+      setFlipped(true);
     }
-  }, [totalCount, monster, setIsFlipped, onFlipped]);
+  }, [interactionCount, totalCount, monster, setFlipped]);
 
   return (
-    <AnimatePresence>
-      <div
-        className="h-[460px] w-[312px]"
-        style={{
-          perspective: '1000px',
-        }}
+    <FlipCard
+      className="m-[5px] h-[460px] w-[312px]"
+      flipped={flipped}
+      onFlipped={onFlipped}
+    >
+      {/* Flip Card Front */}
+      <CardBase
+        type="flip"
+        color={COLOR}
+        className="flex h-full w-full flex-col justify-between overflow-visible"
       >
-        <motion.div
-          className="h-full w-full"
-          initial={false}
-          animate={{ rotateY: isFlipped ? 180 : 360 }}
-          transition={{ duration: 0.85, animationDirection: 'normal' }}
-          onAnimationComplete={() => setIsAnimating(false)}
-          style={{
-            transition: 'transform 0.6',
-            transformStyle: 'preserve-3d',
-          }}
-        >
-          {/* Flip Card Front */}
-          <CardBase
-            type="flip"
-            color={COLOR}
-            className="absolute overflow-visible"
-            style={{
-              backfaceVisibility: 'hidden',
-            }}
+        {/* MonsterCard Main */}
+        <div className="relative h-full w-full rounded-t-[28px] p-16">
+          {!owner && <OwnerInfo name={ownerName} />}
+          <CounterBox
+            startAt={totalCount}
+            endAt={interactionCount}
+            onCount={setTotalCount}
+            onComplete={toggleCard}
+            {...(owner
+              ? {}
+              : { helperText: '화면을 연타하면 사연을 볼 수 있어요' })}
           >
-            <div className="flex h-full w-full flex-col justify-between">
-              <div className="relative h-full w-full overflow-hidden rounded-t-[28px] p-16">
-                <BaseText
-                  component="h3"
-                  variant="label-2"
-                  weight="semibold"
-                  className="mx-auto w-max text-cool-neutral-7"
-                >
-                  {monster?.ownerName}님의 퇴사몬
-                </BaseText>
+            <Image
+              src="/sample.png"
+              width={227}
+              height={192}
+              alt="Sample Monster"
+            />
+          </CounterBox>
+        </div>
+        {/* MonsterCard Bottom */}
+        <div className="flex h-[88px] w-full flex-col gap-12 rounded-b-[28px] bg-cool-neutral-7 px-20 py-12">
+          <div className="flex items-center justify-between">
+            <StressLevelWithName level={ratingRange} name={monsterName} />
+            {owner && (
+              <Dropdown offset={8} placement="top" list={DROPDOWN_LIST} />
+            )}
+          </div>
+          <ProgressBar value={totalCount} max={interactionCount} />
+        </div>
+      </CardBase>
 
-                <CounterBox
-                  startAt={totalCount}
-                  endAt={monster?.interactionCountPerEncouragement || 0}
-                  onCount={setTotalCount}
-                  onComplete={handleCardToggle}
-                  {...(isOwner
-                    ? {}
-                    : { helperText: '화면을 연타하면 사연을 볼 수 있어요' })}
-                >
-                  <Image
-                    src="/sample.png"
-                    width={227}
-                    height={192}
-                    alt="Sample Monster"
-                  />
-                </CounterBox>
-              </div>
-              <div className="flex h-[88px] flex-col gap-12 rounded-b-[28px] bg-cool-neutral-7 p-20">
-                <div className="flex items-center justify-between">
-                  <div className="flex gap-8">
-                    <div className="flex rounded-6 bg-[#70737c47] px-8 py-4">
-                      <BaseText
-                        component="span"
-                        variant="caption-2"
-                        color="neutral"
-                      >
-                        스트레스 {}
-                      </BaseText>
-                    </div>
-                    <BaseText
-                      overflow="truncate"
-                      component="span"
-                      variant="body-1"
-                      weight="semibold"
-                      color="neutral"
-                    >
-                      {monster?.monsterName}
-                    </BaseText>
-                  </div>
-                  {isOwner && <Dropdown />}
-                </div>
-                <ProgressBar
-                  value={totalCount}
-                  max={monster?.interactionCountPerEncouragement}
-                />
-              </div>
-            </div>
-          </CardBase>
-
-          {/* Flip Card Back */}
-          <CardBase
-            role="none" // DO NOT REMOVE!!!
-            type="flip"
-            color={COLOR}
-            className="absolute overflow-visible"
-            onClick={handleCardToggle}
-            style={{
-              transform: 'rotateY(180deg)',
-              backfaceVisibility: 'hidden',
-            }}
-          >
-            <HiddenStory owner={isOwner}>{monster?.content}</HiddenStory>
-          </CardBase>
-        </motion.div>
-      </div>
-    </AnimatePresence>
+      {/* Flip Card Back */}
+      <CardBase type="flip" color={COLOR} className="overflow-visible">
+        <HiddenStory owner={owner} content={content} onClick={toggleCard} />
+      </CardBase>
+    </FlipCard>
   );
 }
