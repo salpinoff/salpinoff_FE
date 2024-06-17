90ce993f421e60cb2ad88fbc77eb833c39ad5116
윤석규 <sg.yun@joongonara.co.kr>
2024-06-17 19:25:21 +0900

    ♻️ refactor: message Query Key Factory 설정

--- src/app/(route)/(monster)/component/Message/MessageList.tsx
+++ src/app/(route)/(monster)/component/Message/MessageList.tsx
@@ -5,9 +5,9 @@ import { useSetAtom } from 'jotai';
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
 import useModal from '@hooks/useModal';
-import useQueryString from '@hooks/useQueryString';
 
 import { getNextMessageList } from '@api/message/list';
+import MessageQueryFactory from '@api/message/query';
 
 import type { Unpromise } from '@type/util';
 
@@ -23,24 +23,28 @@ type LastPage = {
 };
 
 function MessageList() {
-  const [monsterId] = useQueryString('monsterId');
+  const monsterId = '1';
 
   const { openModal, closeModal } = useModal(() => null);
 
   const setTotalElements = useSetAtom(totalMessageAtom);
 
   const {
+    list: { key, fetcher },
+  } = MessageQueryFactory;
+
+  const {
     data: { messageList, totalElements },
   } = useSuspenseInfiniteQuery({
     retry: 1,
     initialPageParam: 1,
-    queryKey: ['message-list', monsterId],
+    queryKey: key({ monsterId }),
+    queryFn: ({ pageParam = 1 }) =>
+      fetcher({ monsterId: Number(monsterId), page: pageParam }),
     select: (pages) => ({
       messageList: pages.pages.map(({ result }) => result.list).flat(),
       totalElements: pages.pages[0].result.totalElements,
     }),
-    queryFn: ({ pageParam = 1 }) =>
-      getNextMessageList({ monsterId: Number(monsterId), page: pageParam }),
     getNextPageParam: ({ nextPage }: LastPage) => {
       return nextPage;
     },
