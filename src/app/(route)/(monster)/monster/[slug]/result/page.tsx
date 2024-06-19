24010068dbb2f9dbf6b7f7eab8a82271da386f8c
emayom <emayom7@gmail.com>
2024-06-20 02:30:36 +0900

    📦 chore: 몬스터 생성 결과 페이지 디렉토리 위치 이동

--- src/app/(route)/(monster)/monster/[slug]/result/page.tsx
+++ src/app/(route)/(monster)/monster/[slug]/result/page.tsx
@@ -4,40 +4,41 @@ import { useRouter } from 'next/navigation';
 
 import { useState, useEffect } from 'react';
 
-import { useQuery } from '@tanstack/react-query';
-
 import { LayoutGroup } from 'framer-motion';
 
 import Button from '@components/common/Button';
 
 import useModal from '@hooks/useModal';
-import useQueryString from '@hooks/useQueryString';
 
-import MONSTER_APIS from '@api/monster';
+import { useGetMonster } from '@api/monster/queries';
 import { GetMonsterResponse } from '@api/monster/types';
 
 import GuideMessage from './components/GuideMessage';
 import MonsterCard from './components/MonsterCard';
 import ShareModal from './components/ShareModal';
 
-export default function ResultSharePage() {
+export default function MonsterResultPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug: monsterId } = params;
+
   const router = useRouter();
-  const [monsterId] = useQueryString('monsterId');
   const [monster, setMonster] = useState<GetMonsterResponse | null>();
 
-  const { error, isError, status, fetchStatus, data } = useQuery({
-    queryKey: ['result', monsterId],
-    queryFn: () => MONSTER_APIS.getMonsterById(monsterId),
-    enabled: !!monsterId,
-    retry: 1,
-    meta: {
-      errorMessage: '몬스터의 정보를 가져오는데 실패했어요!',
+  const { error, isError, status, fetchStatus, data } = useGetMonster(
+    monsterId,
+    {
+      meta: {
+        errorMessage: '몬스터의 정보를 가져오는데 실패했어요!',
+      },
     },
-  });
+  );
 
   useEffect(() => {
     if (data) {
-      setMonster(data.data);
+      setMonster(data);
     }
   }, [data]);
 
@@ -60,7 +61,7 @@ export default function ResultSharePage() {
   }
 
   return (
-    <main className="mx-auto flex h-dvh items-center justify-center bg-gradient-to-b from-cool-neutral-5 to-[#253047]">
+    <div className="mx-auto flex h-full w-full items-center justify-center bg-gradient-to-b from-cool-neutral-5 to-[#253047]">
       <div className="flex h-[573px] max-h-dvh flex-col items-center justify-between py-[20px]">
         {fetchStatus === 'fetching' && <>fetching...</>}
         {status === 'success' && fetchStatus === 'idle' && (
@@ -73,7 +74,7 @@ export default function ResultSharePage() {
               />
             </LayoutGroup>
             <footer className="flex flex-col">
-              <Button size="medium" onClick={openModal}>
+              <Button size="medium" onClick={() => openModal()}>
                 바로 공유하기
               </Button>
               <Button size="medium" variant="ghost" onClick={handleDefer}>
@@ -85,6 +86,6 @@ export default function ResultSharePage() {
 
         {error && <>error</>}
       </div>
-    </main>
+    </div>
   );
 }
