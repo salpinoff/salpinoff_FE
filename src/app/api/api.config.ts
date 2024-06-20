00d07375bb5d003e400b1100a96e43164b0c5db7
윤석규 <sg.yun@joongonara.co.kr>
2024-06-20 22:58:36 +0900

    ✨ feat: fetch api 인스턴스 생성
    - server component 에서 API 호출 시, axios 라이브러리 에러 발생
    - fetch API를 이용해 API 인스턴스 생성

--- src/app/api/api.config.ts
+++ src/app/api/api.config.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+/** axios */
 const createInstance = (baseURL: string) => {
   return axios.create({
     baseURL,
@@ -15,12 +16,56 @@ const apiInstance = createInstance(
   `${process.env.NEXT_PUBLIC_API_DOMAIN_NAME}/api/v1`,
 );
 
+/** fetch */
+const createFetchInstance = (baseUrl: string, options?: RequestInit) => {
+  const defaultHeaders: Record<string, string> = {};
+  const fetchUrl = <T>(path: string, custom?: RequestInit) =>
+    fetch(`${baseUrl}${path}`, {
+      headers: { ...defaultHeaders },
+      ...options,
+      ...custom,
+    }).then((res) => res.json() as Promise<T>);
+
+  return {
+    defaults: {
+      headers: defaultHeaders,
+    },
+    async get<T>(url: string, custom?: RequestInit) {
+      return fetchUrl<T>(url, { ...custom, method: 'get' });
+    },
+    async post(url: string, custom?: RequestInit) {
+      return fetchUrl(url, { ...custom, method: 'post' });
+    },
+    async put(url: string, custom?: RequestInit) {
+      return fetchUrl(url, { ...custom, method: 'put' });
+    },
+    async delete(url: string, custom?: RequestInit) {
+      return fetchUrl(url, { ...custom, method: 'delete' });
+    },
+  };
+};
+
+const baseFetchInstance = createFetchInstance(
+  process.env.NEXT_PUBLIC_DOMAIN_NAME,
+);
+const apiFetchInstance = createFetchInstance(
+  `${process.env.NEXT_PUBLIC_API_DOMAIN_NAME}/api/v1`,
+);
+
 const setAuthHeader = (token: string) => {
   if (token) {
     apiInstance.defaults.headers.Authorization = `Bearer ${token}`;
+    apiFetchInstance.defaults.headers.Authorization = `Bearer ${token}`;
   } else {
     delete apiInstance.defaults.headers.Authorization;
+    delete apiFetchInstance.defaults.headers.Authorization;
   }
 };
 
-export { baseInstance, apiInstance, setAuthHeader };
+export {
+  baseInstance,
+  apiInstance,
+  setAuthHeader,
+  baseFetchInstance,
+  apiFetchInstance,
+};
