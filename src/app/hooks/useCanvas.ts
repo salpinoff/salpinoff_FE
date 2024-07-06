0ed0881c1cda8764cf7438bc6a5a09a8fedac0d1
emayom <emayom7@gmail.com>
2024-07-07 02:03:40 +0900

    🐛 fix: useCanvas 초기 width, height props 받도록 개선

--- src/app/hooks/useCanvas.ts
+++ src/app/hooks/useCanvas.ts
@@ -1,20 +1,21 @@
 import { useEffect, useRef } from 'react';
 
-const useCanvas = () => {
+const useCanvas = (width?: number, height?: number) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const resizeCanvas = () => {
       const canvas = canvasRef.current;
+
       if (canvas) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        canvas.width = width ?? window.innerWidth;
+        canvas.height = height ?? window.innerHeight;
       }
     };
 
     // 초기 캔버스 크기 설정
     resizeCanvas();
-  }, []);
+  }, [width, height]);
 
   return canvasRef;
 };
