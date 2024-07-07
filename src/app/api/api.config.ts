214564e1b6c1827daa28b4e9f3891f0ae8cdcae3
윤석규 <sg.yun@joongonara.co.kr>
2024-07-07 11:04:31 +0900

    ✨ feat: axios interceptor - token refresh
    - request interceptor, 로그인이 필요한 요청에 Authorization 헤더 유무 확인
    - response interceptor, 401 응답 시 토큰 갱신 및 재요청 처리

--- src/app/api/api.config.ts
+++ src/app/api/api.config.ts
@@ -1,4 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+
+import { getQueryClient } from '@utils/query/get-query-client';
+
+import { API_URLS } from './api.constants';
+import { Session } from './schema/token';
 
 /** axios */
 const createInstance = (baseURL: string) => {
@@ -23,5 +28,77 @@ const setAuthHeader = (token: string) => {
     delete apiInstance.defaults.headers.Authorization;
   }
 };
+
+apiInstance.interceptors.request.use(async (request) => {
+  const {
+    REFRESH_TOKEN,
+    GET_TOKEN: { kakao: KAKAO_LOGIN },
+  } = API_URLS.AUTH.API;
+
+  const reqeustUrl = request.url || '';
+  const withOutAuthHeader = [REFRESH_TOKEN, KAKAO_LOGIN];
+
+  const isWithOustUrl = withOutAuthHeader.some((url) =>
+    url.includes(reqeustUrl),
+  );
+
+  if (isWithOustUrl) {
+    delete request.headers.Authorization;
+  }
+
+  if (!isWithOustUrl && !request.headers.Authorization) {
+    const {
+      data: { accessToken },
+    } = await baseInstance.get<Session>(API_URLS.AUTH.BASE.SESSION);
+
+    request.headers.Authorization = `Bearer ${accessToken}`;
+    setAuthHeader(accessToken);
+  }
+
+  return request;
+});
+
+apiInstance.interceptors.response.use(
+  (respone) => respone,
+  async (error: AxiosError) => {
+    const queryClient = getQueryClient();
+    const originalRequest: AxiosRequestConfig & { retry?: boolean } =
+      error.config || { retry: false };
+
+    const isExpireError =
+      error.response?.status === 401 && !originalRequest.retry;
+
+    if (isExpireError) {
+      originalRequest.retry = true;
+
+      try {
+        const {
+          data: { accessToken },
+        } = await baseInstance.post<{ accessToken: string }>(
+          API_URLS.AUTH.BASE.SESSION,
+        );
+
+        queryClient.invalidateQueries({ queryKey: ['authToken'] });
+        setAuthHeader(accessToken);
+
+        originalRequest.headers = {
+          ...originalRequest.headers,
+          Authorization: `Bearer ${accessToken}`,
+        };
+
+        return await apiInstance(originalRequest);
+      } catch (refreshError) {
+        await baseInstance.post(API_URLS.AUTH.BASE.SIGNOUT);
+
+        setAuthHeader('');
+        window.location.href = `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/signin`;
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export { baseInstance, apiInstance, setAuthHeader };
