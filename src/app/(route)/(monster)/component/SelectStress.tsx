519739e34f079f7fcbed2f38e273aa450216b5d2
윤석규 <sg.yun@joongonara.co.kr>
2024-07-08 22:26:58 +0900

    ✨ feat: stress helper text 추가
    - ts-pattern 라이브러리 추가

--- src/app/(route)/(monster)/component/SelectStress.tsx
+++ src/app/(route)/(monster)/component/SelectStress.tsx
@@ -2,6 +2,8 @@
 
 import { ChangeEvent, useEffect } from 'react';
 
+import { match } from 'ts-pattern';
+
 import FormHelperText from '@components/common/TextField/FormHelperText';
 import Slider from '@components/Slider';
 
@@ -13,6 +15,28 @@ const slider = {
   step: 1,
   min: 1,
   max: 100,
+};
+
+/** TODO. helper text 변경하기 */
+const helperText = (stress: number): string => {
+  return match(stress)
+    .when(
+      (value) => value >= 1 && value < 10,
+      () => 'test1',
+    )
+    .when(
+      (value) => value >= 10 && value < 30,
+      () => 'test2',
+    )
+    .when(
+      (value) => value >= 30 && value < 50,
+      () => 'test3',
+    )
+    .when(
+      (value) => value >= 50 && value < 70,
+      () => 'test4',
+    )
+    .otherwise(() => 'test5');
 };
 
 function SelectStress() {
@@ -46,8 +70,9 @@ function SelectStress() {
         onChange={handleChange}
       />
 
-      <span className="m-auto flex h-[180px] w-[180px] items-center justify-center rounded-circular bg-[var(--color-base-cool-neutral-7)]">
+      <span className="m-auto flex h-[180px] w-[180px] flex-col items-center justify-center rounded-circular bg-[var(--color-base-cool-neutral-7)]">
         <span className="display-1-bold text-white">{stress}</span>
+        <FormHelperText>{helperText(stress)}</FormHelperText>
       </span>
     </div>
   );
