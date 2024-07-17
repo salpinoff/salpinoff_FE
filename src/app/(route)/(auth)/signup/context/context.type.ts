7ee608327c06100a6ed5bc49dcf0efb1719bfe17
윤석규 <sg.yun@joongonara.co.kr>
2024-07-17 23:13:20 +0900

    ♻️ refactor: monster info type 분리

--- src/app/(route)/(auth)/signup/context/context.type.ts
+++ src/app/(route)/(auth)/signup/context/context.type.ts
@@ -1,14 +1,9 @@
 import { Member, MemberStatusCodes } from '@api/schema/member';
-import { Decoration, Monster } from '@api/schema/monster';
 
-export type UserInfo = {
+import { MonsterInfo } from 'src/app/(route)/(monster)/monster/produce/context/context.type';
+
+export interface UserInfo extends MonsterInfo {
   // Member Info
   code: MemberStatusCodes;
   userName: Member['username'];
-  // Monster Info
-  emotion: Monster['emotion'] | '';
-  stress: Monster['rating'];
-  story: Monster['content'];
-  monsterName: Monster['monsterName'];
-  decorations: Omit<Decoration, 'decorationId'>[];
-};
+}
