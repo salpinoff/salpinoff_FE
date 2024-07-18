d438f6a578eec7ed11652b4a461fd5bbfd69b928
emayom <emayom7@gmail.com>
2024-07-19 08:26:45 +0900

    🐛 fix: ActionMenu 이미지 저장 기능 관련 메뉴 수정

--- src/app/(route)/(monster)/component/cards/_ui/ActionMenu.tsx
+++ src/app/(route)/(monster)/component/cards/_ui/ActionMenu.tsx
@@ -2,95 +2,97 @@
 
 import { useRouter } from 'next/navigation';
 
-import { useState } from 'react';
-
-import { offset, useFloating } from '@floating-ui/react-dom';
-
 import DownloadSVG from '@public/icons/download.svg';
 import EditSVG from '@public/icons/edit.svg';
 import ShareSVG from '@public/icons/share.svg';
 
+import { type CharacterCanvasProps } from '@components/CharacterCanvas';
 import IconButton from '@components/common/Button/IconButton';
 import Separator from '@components/common/data-display/Separator';
-import { Menu, MenuItem } from '@components/Menu';
+import DropdownMenu from '@components/common/navigation/DropdownMenu';
+import SaveMonsterImageModal from '@components/modals/SaveMonsterImageModal';
 import ShareModal from '@components/modals/ShareModal';
 
 import useModal from '@hooks/useModal';
-import useOutsideClick from '@hooks/useOutsideClick';
 
 import generateShareUrl from '@utils/client/generate-share-url';
+import cn from '@utils/cn';
 
 type ActionMenuProps = {
-  targetId: string;
+  monster: Pick<
+    CharacterCanvasProps,
+    'type' | 'status' | 'items' | 'background'
+  > & {
+    monsterId: string;
+  };
 };
-export default function ActionMenu({ targetId }: ActionMenuProps) {
+export default function ActionMenu({ monster }: ActionMenuProps) {
+  const { monsterId, ...customInfo } = monster;
+
   const router = useRouter();
 
-  const [isOpen, setIsOpen] = useState(false);
+  const { openModal, closeModal } = useModal(() => null);
 
-  const SHARE_URL = generateShareUrl(targetId) ?? '';
-  const MODIFY_URL = `/monster/${targetId}/modify/story`;
-
-  const { openModal, closeModal } = useModal(() => (
-    <ShareModal
-      url={SHARE_URL}
-      onShareByLink={() => {
-        closeModal();
-      }}
-      onShareViaKakao={() => {
-        closeModal();
-      }}
-    />
-  ));
-
-  const { refs, floatingStyles } = useFloating({
-    open: isOpen,
-    placement: 'top',
-    middleware: [offset(8)],
-  });
-
-  useOutsideClick([refs.floating], () => setIsOpen(false), 'mousedown');
-
-  const handleSave = () => {
-    // [TODO]: 저장하기 기능 구현
-    setIsOpen(false);
+  const handleSave = async () => {
+    openModal(() => (
+      <SaveMonsterImageModal
+        monster={customInfo}
+        onClose={() => {
+          closeModal();
+        }}
+        onSave={() => {
+          closeModal();
+        }}
+      />
+    ));
   };
 
   const handleShare = () => {
-    openModal();
-    setIsOpen(false);
+    openModal(() => (
+      <ShareModal
+        url={generateShareUrl(monsterId) ?? ''}
+        onShareByLink={() => {
+          closeModal();
+        }}
+        onShareViaKakao={() => {
+          closeModal();
+        }}
+      />
+    ));
   };
 
   const handleModify = () => {
-    router.push(MODIFY_URL);
-    setIsOpen(false);
+    router.push(`/monster/${monsterId}/modify/story`);
   };
 
   return (
-    <div className="relative w-[55px] text-right shadow-2">
-      <IconButton
-        ref={refs.setReference}
-        id="menu-button"
-        className="inline-flex h-32 w-32 items-center justify-center rounded-circular p-0 text-cool-neutral-40 data-[open]:bg-[#5050501f]"
-        name="ellipsis"
-        onClick={() => setIsOpen((prev) => !prev)}
-      />
-      <Menu open={isOpen} ref={refs.setFloating} style={floatingStyles}>
-        <MenuItem onClick={handleSave}>
+    <DropdownMenu>
+      <DropdownMenu.Trigger>
+        <IconButton
+          id="menu-button"
+          className={cn(
+            'inline-flex h-32 w-32 items-center justify-center rounded-circular p-0 text-cool-neutral-40',
+            'data-[open]:bg-[#5050501f]',
+          )}
+          name="ellipsis"
+        />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content className="w-max rounded-12 bg-[#212225] p-4 text-cool-neutral-99 shadow-2">
+        <DropdownMenu.Item onSelect={handleSave}>
           저장하기
           <DownloadSVG />
-        </MenuItem>
+        </DropdownMenu.Item>
         <Separator />
-        <MenuItem onClick={handleShare}>
+        <DropdownMenu.Item onSelect={handleShare}>
           공유하기
           <ShareSVG />
-        </MenuItem>
+        </DropdownMenu.Item>
         <Separator />
-        <MenuItem onClick={handleModify}>
+        <DropdownMenu.Item onSelect={handleModify}>
           수정하기
           <EditSVG />
-        </MenuItem>
-      </Menu>
-    </div>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu>
   );
 }
