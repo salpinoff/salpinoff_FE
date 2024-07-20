c44e6a91efc69db015894f4e1627797775798d3f
emayom <emayom7@gmail.com>
2024-07-20 15:20:22 +0900

    🐛 fix: localFont fallback 옵션 추가

--- src/app/utils/fonts.ts
+++ src/app/utils/fonts.ts
@@ -2,5 +2,23 @@ import localFont from 'next/font/local';
 
 export const Pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
+  display: 'swap',
   variable: '--font-pretendard',
+  fallback: [
+    'Pretendard Variable',
+    'Pretendard',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
 });
