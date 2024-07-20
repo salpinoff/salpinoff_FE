0fe5f3ba02f52575388372e8ba255e6ecde90571
윤석규 <sg.yun@joongonara.co.kr>
2024-07-20 18:47:44 +0900

    🐛 fix: 슬라이더 간격 수정

--- src/app/(route)/(monster)/monster/produce/components/SelectStress.tsx
+++ src/app/(route)/(monster)/monster/produce/components/SelectStress.tsx
@@ -56,7 +56,7 @@ function SelectStress() {
         open
         label="스트레스 정도"
         content={`스트레스가 높을수록 퇴사몬을 \n 클리어하기 위해 더 많은 탭이 필요해요`}
-        className="mb-[30px]"
+        className="mb-[26px]"
       >
         <Tooltip.Label className="flex-none" />
         <Tooltip.Content className="z-10 p-3" />
