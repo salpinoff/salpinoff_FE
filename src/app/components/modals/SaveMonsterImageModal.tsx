0545f23837399d1bb1f9430a49e5e88433b4e71c
emayom <emayom7@gmail.com>
2024-07-22 00:06:28 +0900

    🐛 fix: SaveMonsterImageModal CSS 수정

--- src/app/components/modals/SaveMonsterImageModal.tsx
+++ src/app/components/modals/SaveMonsterImageModal.tsx
@@ -47,21 +47,20 @@ export default function SaveMonsterImageModal({
   return (
     <Modal open>
       <Modal.Dimmed />
-      <Modal.Content>
-        <Modal.Title>이미지를 저장할까요?</Modal.Title>
-        <div
-          ref={targetRef}
-          className="aspect-square w-full overflow-hidden rounded-8"
-        >
-          <CharacterCanvas
-            className="relative"
-            width={1200}
-            height={1200}
-            type={type}
-            status={status}
-            items={items}
-            background={background}
-          />
+      <Modal.Content className="gap-32 py-32">
+        <Modal.Title align="center">이미지를 저장할까요?</Modal.Title>
+        <div className="aspect-square w-full overflow-hidden rounded-32 shadow-5">
+          <div ref={targetRef}>
+            <CharacterCanvas
+              className="relative"
+              width={1200}
+              height={1200}
+              type={type}
+              status={status}
+              items={items}
+              background={background}
+            />
+          </div>
         </div>
         <div className="flex gap-8">
           <Modal.Button variant="secondary" onClick={onClose} aria-label="닫기">
