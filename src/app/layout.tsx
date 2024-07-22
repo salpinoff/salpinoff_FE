b66cc4130da512eb93d25bec4e0cf5e995e9c842
emayom <emayom7@gmail.com>
2024-07-23 00:03:55 +0900

    📦 chore: meatadata openGraph 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,16 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   title: '퇴사몬',
   description: '스트레스 받을 땐, 참지말고 퇴사몬',
+  openGraph: {
+    siteName: '퇴사몬',
+    title: {
+      default: '퇴사몬',
+      template: '퇴사몬 | %s',
+    },
+    description: '스트레스 받을 땐, 참지말고 퇴사몬',
+    url: process.env.NEXT_PUBLIC_DOMAIN_NAME,
+    locale: 'ko_KR',
+  },
 };
 
 export default function RootLayout({
