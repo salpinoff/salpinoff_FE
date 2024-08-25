d92b36765b0e5982fadd0d437aa7c8ca70ff3044
emayom <emayom7@gmail.com>
2024-08-25 23:23:23 +0900

    🐛 fix: baseLayout 수정

--- src/app/layout/BaseLayout.tsx
+++ src/app/layout/BaseLayout.tsx
@@ -3,49 +3,57 @@ import Image from 'next/image';
 import { PropsWithChildren } from 'react';
 
 import Text from '@components/common/Text';
-import Logo from '@components/Logo';
+
+import cn from '@utils/cn';
+
+import GradientLogoSVG from 'public/icons/gradient-logo.svg';
 
 type BaseLayoutProps = PropsWithChildren;
 
 export default function BaseLayout({ children }: BaseLayoutProps) {
   return (
-    <main className="m-auto flex max-w-[1076px]">
-      <div className="hidden grow select-none flex-col lg:flex">
-        <div className="flex grow flex-col justify-center">
+    <main className="flex">
+      <div className="hidden w-[600px] select-none flex-col bg-gradient-to-b from-[#E6057B] to-[#F450A6] px-[140px] lg:flex">
+        <div className="flex grow flex-col items-center justify-center">
           <Image
-            className="mb-[48px] rounded-20"
+            className="mb-[32px] rounded-[21px]"
             src="/images/app_icon.png"
             alt="앱 아이콘"
-            width={96}
-            height={96}
+            width={80}
+            height={80}
           />
           <Text
-            className="mb-[24px] text-[32px] leading-none"
+            className="mb-20 leading-none"
             component="h3"
-            variant="title-1"
+            variant="heading-1"
             weight="semibold"
             color="strong"
           >
             회사 스트레스 털어놓는 나만의 캐릭터
           </Text>
-          <Logo className="m-0 w-[400px]" />
+          <GradientLogoSVG className="w-[320px]" />
         </div>
         <div className="mb-40">
-          <div className="mb-20 flex gap-12">
-            <div className="h-32 w-32 cursor-pointer rounded-8 bg-white" />
-            <div className="h-32 w-32 cursor-pointer rounded-8 bg-white" />
-          </div>
-          <Text component="span" variant="body-1" color="strong">
+          <Text component="span" variant="label-1" className="text-[#F7F7F8B2]">
             Copyright 2024. Salpinoff. All rights reserved.
           </Text>
         </div>
       </div>
-      <div
-        role="main"
-        id="root-container"
-        className="relative mx-auto mx-auto h-dvh w-full max-w-md grow touch-auto overflow-hidden"
-      >
-        {children}
+      <div className="relative flex h-dvh w-full grow touch-auto items-center justify-center overflow-hidden">
+        <div
+          role="main"
+          id="root-container"
+          className={cn(
+            'relative m-auto box-content h-full w-full',
+            // lg
+            '!h-[970px] !w-[448px] overflow-hidden rounded-32 border-[10px] border-cool-neutral-17'
+              .split(' ')
+              .map((c) => `lg:${c}`)
+              .join(' '),
+          )}
+        >
+          {children}
+        </div>
       </div>
     </main>
   );
