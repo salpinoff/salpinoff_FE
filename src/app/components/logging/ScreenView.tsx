cc4d9c1c6724ebc55b04c680cdf0e823ff1b0816
윤석규 <sg.yun@joongonara.co.kr>
2024-08-30 00:58:58 +0900

    ✨ feat: screenView 이벤트 userProperty 추가
    - 로그인 한 경우, userProperty 를 view_screen 이벤트에 담아서 보내도록 설정

--- src/app/components/logging/ScreenView.tsx
+++ src/app/components/logging/ScreenView.tsx
@@ -2,20 +2,58 @@
 
 import { PropsWithChildren, useEffect } from 'react';
 
+import { useQuery } from '@tanstack/react-query';
+
 import { sendGTMEvent } from '@next/third-parties/google';
+
+import useAuth from '@hooks/api/useAuth';
+
+import { getLocalItem, setLocalItem } from '@utils/local-storage';
+
+import { getMonsterList } from '@api/monster';
+import MonsterQueryFactory from '@api/monster/query/factory';
 
 type Props = PropsWithChildren<{ name: string; disabled?: boolean }>;
 
 function ScreenView({ name, children, disabled = false }: Props) {
+  const { user, status } = useAuth();
+
+  const { data: totalElements } = useQuery({
+    queryKey: MonsterQueryFactory.list.queryKey,
+    queryFn: () => getMonsterList({ page: 1, size: 10 }),
+    enabled: status === 'authenticated',
+    select: (data) => data.data.totalElements,
+  });
+
+  const ensureLoginDate = () => {
+    if (!getLocalItem('loginDate')) {
+      setLocalItem('loginDate', new Date().toISOString());
+    }
+
+    return getLocalItem('loginDate');
+  };
+
   useEffect(() => {
     if (disabled) return;
+    if (
+      status === 'loading' ||
+      (status === 'authenticated' && (!totalElements || !user))
+    )
+      return;
+
+    const userProperties = {
+      userId: user?.id,
+      numOfMonsters: totalElements || -1,
+      lastLoginDate: ensureLoginDate(),
+    };
 
     sendGTMEvent({
       event: 'view_screen',
       screenViewAt: new Date().toISOString(),
       screenName: name,
+      ...(status === 'authenticated' && { ...userProperties }),
     });
-  }, [disabled, name]);
+  }, [disabled, status, totalElements, user]);
 
   return children;
 }
