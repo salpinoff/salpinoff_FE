e377d33d0546327f8c6a0828b13f63012b5577f4
윤석규 <sg.yun@joongonara.co.kr>
2024-08-30 00:38:47 +0900

    ✨ feat: screenview 로깅 추가

--- src/app/(route)/(monster)/component/MainHeader/Setting/drawer-contents/MonsterList.tsx
+++ src/app/(route)/(monster)/component/MainHeader/Setting/drawer-contents/MonsterList.tsx
@@ -11,6 +11,7 @@ import Badge from '@components/common/Badge';
 import Drawer from '@components/common/Drawer';
 import Text from '@components/common/Text';
 import Header from '@components/Header';
+import ScreenView from '@components/Logging/ScreenView';
 import ProgressBar from '@components/ProgressBar';
 
 import useIntersectionObserver from '@hooks/useIntersectionObserver';
@@ -285,68 +286,70 @@ export default function MonsterList() {
   };
 
   return (
-    <AuthSuspense fallback={<>...Loading</>}>
-      <div
-        className={cn(
-          'mx-auto flex h-[calc(100vh-48px)] max-h-full w-max flex-col gap-32 p-20',
-          'snap-y snap-mandatory overflow-y-auto scrollbar-hide',
-        )}
-      >
-        {pages.length &&
-          pages.map((data, idx) => {
-            const { monsterId, monsterName, type, decorations } = data;
-            const { BACKGROUND_COLOR, ...REST_DECOS } = decorations;
+    <ScreenView name="hamburger_monster" disabled={!open}>
+      <AuthSuspense fallback={<>...Loading</>}>
+        <div
+          className={cn(
+            'mx-auto flex h-[calc(100vh-48px)] max-h-full w-max flex-col gap-32 p-20',
+            'snap-y snap-mandatory overflow-y-auto scrollbar-hide',
+          )}
+        >
+          {pages.length &&
+            pages.map((data, idx) => {
+              const { monsterId, monsterName, type, decorations } = data;
+              const { BACKGROUND_COLOR, ...REST_DECOS } = decorations;
 
-            return (
-              <SquareMonsterCard
-                {...(idx === pages.length - 1 ? { ref } : {})}
-                key={monsterId + monsterName}
-                name={monsterName}
-                color={BACKGROUND_COLOR}
-                className={cn(
-                  'relative h-[240px] min-h-[240px] w-[240px]',
-                  'snap-center snap-normal',
-                )}
-                onClick={() => handleClick(idx)}
-              >
-                <CharacterCanvas
-                  width={480}
-                  height={480}
-                  type={type}
-                  items={Object.values(REST_DECOS)}
+              return (
+                <SquareMonsterCard
+                  {...(idx === pages.length - 1 ? { ref } : {})}
+                  key={monsterId + monsterName}
+                  name={monsterName}
+                  color={BACKGROUND_COLOR}
                   className={cn(
-                    'h-full w-full',
-                    type === 'mad' && ' -translate-y-[20px]',
-                    type === 'sad' && ' -translate-y-[10px]',
+                    'relative h-[240px] min-h-[240px] w-[240px]',
+                    'snap-center snap-normal',
                   )}
-                />
-              </SquareMonsterCard>
-            );
-          })}
-        {pages.length && (
-          <DetailDrawer
-            open={open}
-            monster={pages[openId]}
-            closeDrawer={() => setOpen(false)}
-          />
-        )}
-
-        {pages.length === 0 && (
-          <div className="flex h-full w-full flex-col items-center justify-center">
-            <Image
-              width={200}
-              height={200}
-              src="/images/monsters/sad_before0000.png"
-              alt="슬픈 캐릭터"
-              className="opacity-30 grayscale"
+                  onClick={() => handleClick(idx)}
+                >
+                  <CharacterCanvas
+                    width={480}
+                    height={480}
+                    type={type}
+                    items={Object.values(REST_DECOS)}
+                    className={cn(
+                      'h-full w-full',
+                      type === 'mad' && ' -translate-y-[20px]',
+                      type === 'sad' && ' -translate-y-[10px]',
+                    )}
+                  />
+                </SquareMonsterCard>
+              );
+            })}
+          {pages.length && (
+            <DetailDrawer
+              open={open}
+              monster={pages[openId]}
+              closeDrawer={() => setOpen(false)}
             />
+          )}
 
-            <Text variant="body-2" color="alternative">
-              아직 완료된 퇴사몬이 없어요
-            </Text>
-          </div>
-        )}
-      </div>
-    </AuthSuspense>
+          {pages.length === 0 && (
+            <div className="flex h-full w-full flex-col items-center justify-center">
+              <Image
+                width={200}
+                height={200}
+                src="/images/monsters/sad_before0000.png"
+                alt="슬픈 캐릭터"
+                className="opacity-30 grayscale"
+              />
+
+              <Text variant="body-2" color="alternative">
+                아직 완료된 퇴사몬이 없어요
+              </Text>
+            </div>
+          )}
+        </div>
+      </AuthSuspense>
+    </ScreenView>
   );
 }
--- src/app/(route)/(monster)/component/MainHeader/Setting/index.tsx
+++ src/app/(route)/(monster)/component/MainHeader/Setting/index.tsx
@@ -5,6 +5,7 @@ import { MouseEventHandler, ReactElement, useState } from 'react';
 import Drawer from '@components/common/Drawer';
 import Menu from '@components/common/Menu';
 import Header from '@components/Header';
+import ScreenView from '@components/Logging/ScreenView';
 import SignoutConfirmModal from '@components/modals/SignoutConfirmModal';
 
 import useModal from '@hooks/useModal';
@@ -60,76 +61,78 @@ export default function Setting({ open = false, close }: SettingProps) {
   };
 
   return (
-    <Drawer open={open} className="">
-      <Header className="grid grid-cols-6 gap-4">
-        <Header.IconButton
-          name="arrow-back"
-          aria-label="뒤로가기"
-          className="col-span-1 col-start-1"
-          onClick={close}
-        />
-        <Header.Title className="col-span-4 col-start-2 mx-auto">
-          설정
-        </Header.Title>
-      </Header>
-      {/* Menu Group */}
-      <div className="p-20">
-        <Menu>
-          <Menu.Item
-            type="button"
-            component="button"
-            onClick={handleViewCollection}
-            aria-label="퇴사몬 보관함"
-            data-label="퇴사몬 보관함"
-          >
-            퇴사몬 보관함
-          </Menu.Item>
-        </Menu>
-
-        <Menu className="my-16">
-          <Menu.Item
-            type="button"
-            component="button"
-            onClick={handleEditProfile}
-            aria-label="프로필 수정"
-            data-label="프로필 수정"
-          >
-            프로필 수정
-          </Menu.Item>
-          <Menu.Item
-            type="button"
-            component="button"
-            loading={isPending}
-            onClick={handleSignout}
-            aria-label="로그아웃"
-            data-label="로그아웃"
-          >
-            로그아웃
-          </Menu.Item>
-        </Menu>
-
-        <Menu>
-          <Menu.Item component="a" href="/" data-label="제작정보">
-            제작정보
-          </Menu.Item>
-        </Menu>
-      </div>
-
-      {/* SubDrawer */}
-      <Drawer open={subOpen}>
+    <ScreenView name="hamburger" disabled={!open}>
+      <Drawer open={open} className="">
         <Header className="grid grid-cols-6 gap-4">
           <Header.IconButton
             name="arrow-back"
             aria-label="뒤로가기"
             className="col-span-1 col-start-1"
-            onClick={() => setSubOpen(false)}
+            onClick={close}
           />
           <Header.Title className="col-span-4 col-start-2 mx-auto">
-            {subContentLabel}
+            설정
           </Header.Title>
         </Header>
-        {subContent}
+        {/* Menu Group */}
+        <div className="p-20">
+          <Menu>
+            <Menu.Item
+              type="button"
+              component="button"
+              onClick={handleViewCollection}
+              aria-label="퇴사몬 보관함"
+              data-label="퇴사몬 보관함"
+            >
+              퇴사몬 보관함
+            </Menu.Item>
+          </Menu>
+
+          <Menu className="my-16">
+            <Menu.Item
+              type="button"
+              component="button"
+              onClick={handleEditProfile}
+              aria-label="프로필 수정"
+              data-label="프로필 수정"
+            >
+              프로필 수정
+            </Menu.Item>
+            <Menu.Item
+              type="button"
+              component="button"
+              loading={isPending}
+              onClick={handleSignout}
+              aria-label="로그아웃"
+              data-label="로그아웃"
+            >
+              로그아웃
+            </Menu.Item>
+          </Menu>
+
+          <Menu>
+            <Menu.Item component="a" href="/" data-label="제작정보">
+              제작정보
+            </Menu.Item>
+          </Menu>
+        </div>
+
+        {/* SubDrawer */}
+        <Drawer open={subOpen}>
+          <Header className="grid grid-cols-6 gap-4">
+            <Header.IconButton
+              name="arrow-back"
+              aria-label="뒤로가기"
+              className="col-span-1 col-start-1"
+              onClick={() => setSubOpen(false)}
+            />
+            <Header.Title className="col-span-4 col-start-2 mx-auto">
+              {subContentLabel}
+            </Header.Title>
+          </Header>
+          {subContent}
+        </Drawer>
       </Drawer>
-    </Drawer>
+    </ScreenView>
   );
 }
