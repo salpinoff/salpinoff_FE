4e0166e61bbd42dfe557b1f4858adcfe537742ca
emayom <emayom7@gmail.com>
2024-09-02 16:46:12 +0900

    🐛 fix: 온보딩 디자인 수정

--- src/app/(route)/onboarding/constants/funnel.ts
+++ src/app/(route)/onboarding/constants/funnel.ts
@@ -3,8 +3,6 @@ export const funnel = [
     name: 'intro_concept',
     title: '회사에서 받은 스트레스와 감정을\n 퇴사몬에 담아보세요',
     image: {
-      width: 263,
-      height: 398,
       alt: '퇴사몬 만들기 페이지 예시 이미지',
       src: '/images/onboarding/intro_concept.png',
     },
@@ -13,8 +11,6 @@ export const funnel = [
     name: 'customize_feature',
     title: '다양한 아이템을 활용하여\n 나만의 퇴사몬을 꾸며보세요',
     image: {
-      width: 338,
-      height: 398,
       alt: '퇴사몬 꾸미기 페이지 예시 이미지',
       src: '/images/onboarding/customize_feature.png',
     },
@@ -23,8 +19,6 @@ export const funnel = [
     name: 'share_feature',
     title: '친구들에게 퇴사몬을 공유하고\n 응원 메시지와 에너지를 받아보세요',
     image: {
-      width: 263,
-      height: 398,
       alt: '퇴사몬 공유 페이지 예시 이미지',
       src: '/images/onboarding/share_feature.png',
     },
@@ -34,8 +28,6 @@ export const funnel = [
     title:
       '받은 에너지로 화난 퇴사몬은 무찌르고,\n 슬픈 퇴사몬은 행복하게 해주세요',
     image: {
-      width: 263,
-      height: 300,
       alt: '퇴사몬 인터랙션 가이드 이미지',
       src: '/images/onboarding/interaction_feature.png',
     },
@@ -44,8 +36,6 @@ export const funnel = [
     name: 'store_feature',
     title: '완료된 퇴사몬은 보관함에 저장하고\n 다양한 퇴사몬을 만들어보세요',
     image: {
-      width: 263,
-      height: 400,
       alt: '퇴사몬 보관함 페이지 예시 이미지',
       src: '/images/onboarding/store_feature.png',
     },
